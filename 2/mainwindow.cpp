cdd575b990390e5420a43c4faefd26d58c5f78a4
Heather <heather@live.ru>
2015-03-23 20:07:13 +0400

    +++

--- 2/mainwindow.cpp
+++ 2/mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QFileDialog>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -11,4 +13,29 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_toolButton_clicked()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"));
+    ui->lineEdit->setText(fileName);
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    auto fname = ui->lineEdit->text();
+    auto item = new QListWidgetItem(fname);
+    ui->listWidget->addItem(item);
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    ; //
+}
+
+void MainWindow::on_buttonBox_accepted()
+{
+    QListWidgetItem* fname = ui->listWidget->selectedItems().at(0);
+    QString quotedFname = "cmd /c \"" + fname->text() + "\"";
+    system( quotedFname.toStdString().c_str() );
 }
--- 2/mainwindow.h
+++ 2/mainwindow.h
@@ -15,6 +15,15 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+  void on_toolButton_clicked();
+
+  void on_pushButton_2_clicked();
+
+  void on_pushButton_clicked();
+
+  void on_buttonBox_accepted();
+
 private:
     Ui::MainWindow *ui;
 };
