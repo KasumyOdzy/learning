f9a4ef7060263bcac41cd72670fb5cf61e2d1668
Heather <heather@live.ru>
2015-03-26 17:11:46 +0400

    fix

--- 2/mainwindow.cpp
+++ 2/mainwindow.cpp
@@ -10,51 +10,56 @@
 #include <QTextStream>
 
 MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+  QMainWindow(parent),
+  ui(new Ui::MainWindow)
 
 {
-    ui->setupUi(this);
+  ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
 {
-        delete ui;
+  delete ui;
 }
 
 void MainWindow::on_toolButton_clicked()
 {
-    QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"));
-    ui->lineEdit->setText(fileName);
+  QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"));
+  ui->lineEdit->setText(fileName);
 }
 
 void MainWindow::on_pushButton_2_clicked()
 {
-    auto fname = ui->lineEdit->text();
-    auto item = new QListWidgetItem(fname);
-    ui->listWidget->addItem(item);
-    QFile mfile("//mfile.txt");
-    QTextStream stream(&mfile);
-    stream << ui->lineEdit->toPlainText();
-    mfile close();
-
+  auto fname = ui->lineEdit->text();
+  auto item = new QListWidgetItem(fname);
+  ui->listWidget->addItem(item);
+  QFile mfile("//mfile.txt");
+  mfile.open(QIODevice::WriteOnly | QIODevice::Text);
+  QTextStream stream(&mfile);
+  stream << ui->lineEdit->text();
+  mfile.close();
 }
 
 void MainWindow::on_pushButton_clicked()
 {
-   /* auto item = ui->listWidget->fname();
+  /* auto item = ui->listWidget->fname();
     auto fname = new QListWidgetItem(item);
     ui->listWidget->deleteLater();*/
 }
 
 void MainWindow::on_pushButton_3_clicked()
 {
-    QListWidgetItem* fname = ui->listWidget->selectedItems().at(0);
-    QString quotedFname = "cmd /c \"" + fname->text() + "\"";
-    system( quotedFname.toStdString().c_str() );
+  QListWidgetItem* fname = ui->listWidget->selectedItems().at(0);
+  QString quotedFname = "cmd /c \"" + fname->text() + "\"";
+  system( quotedFname.toStdString().c_str() );
 }
 
 void MainWindow::on_pushButton_4_clicked()
 {
-    close();
+  close();
+}
+
+void MainWindow::on_actionRead_triggered()
+{
+
 }
